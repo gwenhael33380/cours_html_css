8dbe3af416f8cce932eb634db3bb1bdbdb871b4d
Le Corre <le.corre.gwen.hael@gmail.com>
2022-03-08 13:02:24 +0100

    push

---
+++ submit_form.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Formulaire soumis</h1>
+	<?php var_dump($_POST) ?>
+</body>
+</html>
